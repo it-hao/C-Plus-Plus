eb62c9d440467625f319690b03a41f1f33fe3d85
it-hao <haoshenhs@gmail.com>
2020-03-01 17:55:18 +0800

    submit selectsort.cpp

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,51 @@
+// C++ program to implement Selection sort 
+#include <iostream>
+using namespace std; 
+  
+void SelectSort(int *a, int n) 
+{
+	for(int i = 0; i < n; i ++) 
+	{
+		int minIndex = i;
+		for(int j = i + 1; j < n; j ++)
+		{
+			if(a[minIndex] > a[j]){
+				minIndex = j;
+			}
+		}
+		swap(a[i], a[minIndex]);
+	}
+} 
+
+void show(int *a, int len)
+{
+    int i;
+    for (i = 0; i < len; i++)
+        cout << a[i] << "\n";
+}
+
+// driver function to test the algorithm 
+int main() 
+{ 
+	int len;
+    cout << "\nEnter the number of elements : ";
+
+    cin >> len;
+
+    int arr[len];
+
+    cout << "\nEnter the unsorted elements : ";
+
+    for (int i = 0; i < len; ++i)
+    {
+        cout << "\n";
+        cin >> arr[i];
+    }
+    
+    SelectSort(arr, len); 
+  
+    cout << "Sorted array\n";
+    show(arr, len); 
+  
+    return 0; 
+} 
