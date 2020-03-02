6120076495cf48101e3d3f92c1b784acd31f045d
it-hao <haoshenhs@gmail.com>
2020-03-02 23:59:49 +0800

    修改快速排序的partion方法

--- Sorting/Quick Sort.cpp
+++ Sorting/Quick Sort.cpp
@@ -4,21 +4,44 @@ using namespace std;
 
 int partition(int *a, int low, int high)
 {
-	int pivot = a[low];
-	
-	while(low < high)
+	int i, j;
+	i = j = low;
+	int pivot = a[high];
+	// 通过游标i进而将a[low, high-1]分成两部分，一部分是已经处理的，都小于 pivot 的, 即：a[low, i-1]
+	// 另一部分是处未理的，即：a[i, high-1]	我们每次都从未处理的区域取出一个去和 pivot 比较， 如果小
+	// 于的话，就将其放到处理区域的尾部，最后 i 就是那个分界点。 
+	for(; j < high; j ++)
 	{
-		while (low < high && a[high] > pivot)	high --;
-		a[low] = a[high];
-		
-		while (low < high && a[low] < pivot)		low ++;
-		a[high] = a[low];
+		if(a[j] < pivot)
+		{
+			if(i != j)
+			{
+				swap(a[i], a[j]);
+			}
+			i ++;
+		}
 	}
-	
-	a[low] = pivot;
-	return low;
-   
+	swap(a[i], a[high]);
+	return i;
 }
+
+//int partition(int *a, int low, int high)
+//{
+//	int pivot = a[low];
+//	
+//	while(low < high)
+//	{
+//		while (low < high && a[high] > pivot)	high --;
+//		a[low] = a[high];
+//		
+//		while (low < high && a[low] < pivot)		low ++;
+//		a[high] = a[low];
+//	}
+//	
+//	a[low] = pivot;
+//	return low;
+//   
+//}
 
 void quickSort(int *a, int low, int high)
 {
